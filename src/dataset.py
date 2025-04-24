5d17c8d09d3ff304034249000ca70641dcfc7815
tenyi3323 <tenzinyima257@gmail.com>
2025-04-25 00:47:20 +0100

    Add initial notebook and dataset creation script

---
+++ src/dataset.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# 2. Creating a DataFrame
+data = {
+    "Name": ["Alice", "Bob", "Charlie", "David"],
+    "Age": [24, 27, 22, 32],
+    "City": ["New York", "Los Angeles", "Chicago", "Houston"],
+}
+df = pd.DataFrame(data)
+df.head(2)
+df.replace("Alice", "Tenzin")
